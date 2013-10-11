b18ba6a5987d0c0b1e7771f905f5a0d192b00c02
Ben Edwards <edwards.benj@gmail.com>
2013-10-11 10:21:04 +0100

    initialising repo

---
+++ src/Main.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE TypeOperators #-}
+
+module Main 
+  where
+
+data Fix f = In { out :: f (Fix f) }
+
+data PrimLogoF r = FD Int r | RT Int r | End
+data SugarLogoF r = Repeat Int r
+
+instance Functor PrimLogoF where
+    fmap f (FD x r) = FD x $ f r
+    fmap f (RT x r) = RT x $ f r
+    fmap _ End = End
+
+instance Functor SugarLogoF where
+    fmap f (Repeat n rs) = Repeat n $ fmap f rs
+
+type PrimLogo = Fix PrimLogoF
+type SugarLogo = Fix PrimLogoF
+
+data (f :+: g) e = InL (f e) | InR (g e)
+
+instance (Functor f, Functor g) => Functor (f :+: g) where
+    fmap fn (InL f) = InL $ fmap fn f
+    fmap fn (InR g) = InR $ fmap fn g
+
+type LogoF = PrimLogoF :+: SugarLogoF
+type Logo = Fix LogoF
+
+type Algebra f a = f a -> a
+
+cata :: Functor f => Algebra f a -> Fix f -> a
+cata f = f . fmap (cata f) . out
+
+unroll :: Logo -> PrimLogo
+unroll (In (InL (FD x is))) = In $ FD x (unroll is)
+unroll (In (InL (RT x is))) = In $ RT x (unroll is)
+unroll (In (InL (End)))     = In $ End
+unroll (In (InR (Repeat n is))) = In $ End
+
+
+-- Smart Constructors
+fd :: Int -> Logo -> Logo
+fd dist tail = (In . InL) $ FD dist tail
+
+rt :: Int -> Logo -> Logo
+rt ang tail = (In . InL) $ RT ang tail
+
+end :: Logo
+end = In . InL $ End
+
+prog :: Logo
+prog = fd 10 (rt 90 end)
+
+
+main :: IO ()
+main = putStrLn "test"
+
+
+
